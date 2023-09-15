3a2493a2c8d105d842ce68e58ea3f8ee8bb0fb2e
mbrenn <mbrenn@vt.edu>
2023-09-15 19:45:17 -0400

    created initial project skeleton

---
+++ vthacks/vthacks/src/main/java/edu/vt/vthacks/VthacksApplication.java
@@ -0,0 +1,13 @@
+package edu.vt.vthacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VthacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VthacksApplication.class, args);
+	}
+
+}
