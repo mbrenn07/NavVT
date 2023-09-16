3796a8bf37d1c4f3524762eff5911e6476cab0d7
mbrenn <mbrenn@vt.edu>
2023-09-16 15:37:51 -0400

    added displayed routes

--- navigator/src/BTMap.js
+++ navigator/src/BTMap.js
@@ -1,5 +1,5 @@
 import { GoogleMap, MarkerF, useLoadScript, PolylineF } from "@react-google-maps/api";
-import { useMemo, useState, useEffect } from "react";
+import { useMemo, useState, useEffect, useRef } from "react";
 import BackendService from "./BackendService.js";
 
 import "./App.css";
@@ -13,23 +13,22 @@ const App = () => {
   const [buses, setBuses] = useState([]);
   const [busToStop, setbusToStop] = useState([]);
   const [stopCodeToBus, setStopCodeToBus] = useState({});
+  const [busLines, setBusLines] = useState([]);
+
+  const busLength = useRef(0);
+  busLength.current = 0;
 
   useEffect(() => {
     BackendService.getActiveBusInfo().then((response) => {
       setBuses(response.data.data);
+      busLength.current = response.data.data;
       //use buses[x].states[0] to get direction, speed, capacity, passengers, lat, long
       createBusRoutes(response.data.data);
     }
     );
     const interval = setInterval(() => {
       BackendService.getActiveBusInfo().then((response) => {
-        let busPrevLength = buses.length;
         setBuses(response.data.data);
-        console.log(stopCodeToBus);
-        //use buses[x].states[0] to get direction, speed, capacity, passengers, lat, long
-        if (response.data.data.length !== busPrevLength) {
-          createBusRoutes(response.data.data);
-        }
       }
       );
     }, 5000)
@@ -37,20 +36,34 @@ const App = () => {
   }, [])
 
   const createBusRoutes = (newBuses) => {
+    const busLines = [];
     newBuses.forEach((bus) => {
+      const waypointCoords = [];
       BackendService.getBusRoute(bus.patternName)
-      .then((data) => {
-        //process data into stopIdToBus
-        data.data.data?.forEach((stop) => {
-          if (stop.isBusStop === "Y") {
-            stopCodeToBus[stop.stopCode] = stopCodeToBus[stop.stopCode] ? stopCodeToBus[stop.stopCode].add(bus.routeId) : new Set([bus.routeId]);
-            busToStop[bus.routeId] = busToStop[bus.routeId] ? busToStop[bus.routeId].add(stop) : new Set([stop]);
-          }
-        });
-        setStopCodeToBus({...stopCodeToBus});
-        setbusToStop({...busToStop});
-      })
-      .catch((e) => console.error(e));
+        .then((data) => {
+          //process data into stopIdToBus
+          data.data.data?.forEach((stop) => {
+            if (stop.isBusStop === "Y") {
+              stopCodeToBus[stop.stopCode] = stopCodeToBus[stop.stopCode] ? stopCodeToBus[stop.stopCode].add(bus.routeId) : new Set([bus.routeId]);
+              busToStop[bus.routeId] = busToStop[bus.routeId] ? busToStop[bus.routeId].add(stop) : new Set([stop]);
+            }
+            waypointCoords.push({ lat: parseFloat(stop.latitude), lng: parseFloat(stop.longitude) });
+          });
+          setStopCodeToBus({ ...stopCodeToBus });
+          setbusToStop({ ...busToStop });
+          busLines.push(
+            <PolylineF
+              path={waypointCoords}
+              options={{
+                strokeColor: "#FF0000",
+                strokeOpacity: 1.0,
+                strokeWeight: 2,
+              }}
+            />
+          );
+          setBusLines([...busLines]);
+        })
+        .catch((e) => console.error(e));
     });
   }
 
@@ -64,10 +77,10 @@ const App = () => {
           center={center}
           zoom={14}
         >
-
+          {busLines}
           {buses.length > 0 && (
             buses.map((b, i) => {
-              return (<MarkerF key={i} position={{ lat: b.states[0].latitude , lng: b.states[0].longitude }}
+              return (<MarkerF key={i} position={{ lat: b.states[0].latitude, lng: b.states[0].longitude }}
                 icon={
                   {
                     path: "M21 3L3 10.53v.98l6.84 2.65L12.48 21h.98L21 3z",
@@ -75,7 +88,7 @@ const App = () => {
                     strokeColor: "#000000",
                     fillColor: "#000000",
                     fillOpacity: 1,
-                    anchor: {x: 10, y: 10},
+                    anchor: { x: 10, y: 10 },
                     rotation: -45 + parseInt(b.states[0].direction),
                   }} />)
             })
