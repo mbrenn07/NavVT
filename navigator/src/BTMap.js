74a47a7d17f7ea68e77f8c81b1518301d42cdb76
Erik Robbins <erikr19@vt.edu>
2023-09-16 16:44:48 -0400

    Added stop markers

--- navigator/src/BTMap.js
+++ navigator/src/BTMap.js
@@ -48,6 +48,7 @@ const App = () => {
             }
             waypointCoords.push({ lat: parseFloat(stop.latitude), lng: parseFloat(stop.longitude) });
           });
+          console.log(stopCodeToBus);
           setStopCodeToBus({ ...stopCodeToBus });
           setbusToStop({ ...busToStop });
           busLines.push(
@@ -93,13 +94,16 @@ const App = () => {
             })
           )}
           {/* Put routes.map here to render bus stop marker data */}
-          {/* {busToStop.length > 0 && (
-            busToStop.map((bus, i) => {
-              return (
-                <MarkerF key={i} position={{ lat: b.states[0].latitude , lng: b.states[0].longitude }} />
-              )
-            })
-          )} */}
+          {stopCodeToBus && Object.values(stopCodeToBus).map((val, i) => {
+             return(val.stop && <MarkerF key={i} position={{ lat: parseFloat(val.stop.latitude), lng: parseFloat(val.stop.longitude) }} 
+             icon = {{path: "M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 400Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Z",
+             scale: 0.02,
+             strokeColor: "#000000",
+             fillColor: "#000000",
+             fillOpacity: 1, }} />)
+
+          })
+          }
 
           {/* <MarkerF position={{ lat: 37.2269965, lng: -80.4113475 }} 
           icon = {{url: require('./navigation.svg').default,}}/>
