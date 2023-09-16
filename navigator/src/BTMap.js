e8af96a31c7cd1e9a53ae88cda68710dc1d885f2
mbrenn <mbrenn@vt.edu>
2023-09-16 13:49:37 -0400

    changed anchor point

--- navigator/src/BTMap.js
+++ navigator/src/BTMap.js
@@ -18,20 +18,18 @@ const App = () => {
       //use buses[x].states[0] to get direction, speed, capacity, passengers, lat, long
     }
     );
+
+    setInterval(() => {
+      getActiveBusInfo().then((response) => {
+        setBuses(response.data.data);
+        //use buses[x].states[0] to get direction, speed, capacity, passengers, lat, long
+      }
+      );
+    }, 5000)
   }, [])
-
-  setInterval(() => {
-    BackendService.getBTInfo().then((response) => {
-      setBuses(response.data.data);
-      //use buses[x].states[0] to get direction, speed, capacity, passengers, lat, long
-    }
-    );
-  }, 5000)
-
 
   const marker1Position = { lat: 37.2269965, lng: -80.4113475 };
   const marker2Position = { lat: 37.230000, lng: -80.420000 };
-  
 
   return (
     <>
@@ -45,20 +43,21 @@ const App = () => {
         >
 
           {buses.length > 0 && (
-            buses.map((b, i) => {  
-              return (<MarkerF key={i} position={{ lat: b.states[0].latitude, lng: b.states[0].longitude }} 
+            buses.map((b, i) => {
+              return (<MarkerF key={i} position={{ lat: b.states[0].latitude , lng: b.states[0].longitude }}
                 icon={
                   {
-                      path: "M21 3L3 10.53v.98l6.84 2.65L12.48 21h.98L21 3z",
-                      scale: 1,
-                      strokeColor: "#000000",
-                      fillColor: "#000000",
-                      fillOpacity: 1,
-                      rotation: -45+parseInt(b.states[0].direction),
+                    path: "M21 3L3 10.53v.98l6.84 2.65L12.48 21h.98L21 3z",
+                    scale: 1.25,
+                    strokeColor: "#000000",
+                    fillColor: "#000000",
+                    fillOpacity: 1,
+                    anchor: {x: 10, y: 10},
+                    rotation: -45 + parseInt(b.states[0].direction),
                   }} />)
             })
           )}
-          
+
           {/* <MarkerF position={{ lat: 37.2269965, lng: -80.4113475 }} 
           icon = {{url: require('./navigation.svg').default,}}/>
           <MarkerF position={{ lat: 37.230000, lng: -80.420000 }} 
