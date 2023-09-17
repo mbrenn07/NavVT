997d77fef4e7c51880a440cbb7a96d0b75ab7784
mbrenn <mbrenn@vt.edu>
2023-09-17 02:22:00 -0400

    fixed flickering bug

--- navigator/src/BTMap.js
+++ navigator/src/BTMap.js
@@ -142,8 +142,7 @@ const App = () => {
                   </linearGradient></defs> \
                   <path fill="url(%23mygx)" stroke-width="1.5" d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 400Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Z"></path> \
                   </svg>';
-                console.log(xyz)
-              return (val.stop && <MarkerF key={Math.random()} position={{ lat: parseFloat(val.stop.latitude), lng: parseFloat(val.stop.longitude) }}
+              return (val.stop && <MarkerF key={val.stop.stopCode + routeIds[0] + routeIds[1] + routeIds[2] + routeIds[3]} position={{ lat: parseFloat(val.stop.latitude), lng: parseFloat(val.stop.longitude) }}
                 icon={{
                   url: xyz,
                 }} />)
